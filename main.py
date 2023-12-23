2bf73c60e0cce40b111c536de42a90f39ed41da8
GeorgeDenis <george.duluta@yahoo.com>
2023-12-23 14:41:06 +0200

    Fetching data from a partition, table view of data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, render_template
+import datetime
+import os
 import psutil
 
 app = Flask(__name__)
@@ -14,6 +16,28 @@ def getPartitionsId():
         return jsonify(error=str(e)), 500
 
     return jsonify({'partitions': partitions}), 200
+
+
+@app.route('/<partition>', methods=['GET'])
+def show_partition(partition):
+    base_path = f"{partition}://"
+    files = {}
+    exclusions = {'$RECYCLE.BIN', '$Recycle.Bin', '.GamingRoot', 'System Volume Information'}
+
+    try:
+        for name in os.listdir(base_path):
+            if name in exclusions:
+                continue
+            path = os.path.join(base_path, name)
+            file_stats = os.stat(path)
+            extension = os.path.splitext(name)[1]
+            size = 0 if os.path.isdir(path) else file_stats.st_size
+            date = datetime.datetime.fromtimestamp(file_stats.st_ctime).strftime('%Y-%m-%d %H:%M')
+            files[name] = {'path': path, 'extension': extension, 'size': size, 'date': date}
+    except Exception as e:
+        return jsonify({"error": f"Nu s-a putut accesa partiția {partition}: {e}"}), 500
+
+    return jsonify({"folders": files}), 200
 
 
 @app.route('/')
--- static/scripts.js
+++ static/scripts.js
@@ -1,12 +1,6 @@
 const panel1List = document.getElementById("panel1List")
 const panel2List = document.getElementById("panel2List")
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadPartitionList(panel1List);
-  loadPartitionList(panel2List);
-});
-
-
 async function loadPartitionList(panel) {
   const response = await fetch("/partitions");
   const data = await response.json();
@@ -18,4 +12,79 @@ async function loadPartitionList(panel) {
   });
 }
 
+function loadPartitionData(panelId, partition) {
+  fetch(`/${partition}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const panel = document.getElementById(panelId);
+      let tableBody = panel.getElementsByTagName("tbody")[0];
+
+      if (data.error) {
+        tableBody.innerHTML = `<tr><td colspan="4">Error: ${data.error}</td></tr>`;
+      } else {
+        let content = "";
+        for (const [name, info] of Object.entries(data.folders)) {
+          content += `<tr>
+                                  <td>${name}</td>
+                                  <td>${info.extension || ""}</td>
+                                  <td>${info.size}</td>
+                                  <td>${info.date}</td>
+                              </tr>`;
+        }
+        tableBody.innerHTML = content;
+
+        const otherTableId = panelId === "panel1" ? "panel2" : "panel1";
+        addRowSelectionListeners(tableBody, otherTableId);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      tableBody.innerHTML = `<tr><td colspan="4">Error loading data: ${error}</td></tr>`;
+    });
+}
+
+function initPartitionSelectors() {
+  Promise.all([
+    loadPartitionList(panel1List),
+    loadPartitionList(panel2List),
+  ]).then(() => {
+    panel1PartitionSelector();
+    panel2PartitionSelector();
+  });
+
+  panel1List.addEventListener("change", panel1PartitionSelector);
+  panel2List.addEventListener("change", panel2PartitionSelector);
+}
+function panel1PartitionSelector() {
+  if (panel1List.options.length > 0) {
+    const selectedPartition = panel1List.value;
+    loadPartitionData("panel1", selectedPartition);
+  }
+}
+
+function panel2PartitionSelector() {
+  if (panel2List.options.length > 0) {
+    const selectedPartition = panel2List.value;
+    loadPartitionData("panel2", selectedPartition);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initPartitionSelectors();
+});
+
+function addRowSelectionListeners(tableBody, otherTableId) {
+  tableBody.querySelectorAll("tr").forEach((row) => {
+    row.addEventListener("click", () => {
+      const otherTableBody = document
+        .getElementById(otherTableId)
+        .getElementsByTagName("tbody")[0];
+      const selectedInOtherTable = otherTableBody.querySelector(".selected");
+
+      if (!selectedInOtherTable) {
+        row.classList.toggle("selected");
+      }
+    });
+  });
+}
 
