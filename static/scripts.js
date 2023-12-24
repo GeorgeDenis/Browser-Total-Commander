018b3203012d85ec3fc10c3ff692230a2788035b
GeorgeDenis <george.duluta@yahoo.com>
2023-12-24 11:16:56 +0200

    Added folder name input for creating a folder, fixed rerendering for adding a new folder, deleting data

--- static/scripts.js
+++ static/scripts.js
@@ -1,8 +1,45 @@
-const panel1List = document.getElementById("panel1List")
-const panel2List = document.getElementById("panel2List")
-let pathPanel1 = ''
-let pathPanel2 = ''
-const otherTableId = '';
+const panel1List = document.getElementById("panel1List");
+const panel2List = document.getElementById("panel2List");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".btn-close");
+const createFolderPanel1 = document.getElementById("createPanel1");
+const createFolderPanel2 = document.getElementById("createPanel2");
+const submitCreateFolder = document.getElementById("btn-submit-folder");
+
+let pathPanel1 = "";
+let pathPanel2 = "";
+const otherTableId = "";
+
+let modalPanelId = "";
+
+const openModal = function (panelId) {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  modalPanelId = panelId;
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  modalPanelId = "";
+};
+
+createFolderPanel1.addEventListener("click", () => openModal("panel1"));
+createFolderPanel2.addEventListener("click", () => openModal("panel2"));
+
+function createFolderInput(){
+  const folderValue = document.getElementById("folder").value
+  createFolder(modalPanelId,folderValue)
+  closeModal()
+}
+
+
+
+
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
 async function loadPartitionList(panel) {
   const response = await fetch("/partitions");
   const data = await response.json();
@@ -37,10 +74,10 @@ function loadPartitionData(panelId, partition) {
 
         const otherTableId = panelId === "panel1" ? "panel2" : "panel1";
         addRowSelectionListeners(tableBody, otherTableId);
-        if(panelId === "panel1"){
-            pathPanel1 = partition;
-        }else{
-            pathPanel2 = partition;
+        if (panelId === "panel1") {
+          pathPanel1 = partition;
+        } else {
+          pathPanel2 = partition;
         }
       }
     })
@@ -91,11 +128,11 @@ function addRowSelectionListeners(tableBody, otherTableId) {
         row.classList.toggle("selected");
       }
     });
-    row.addEventListener("dblclick", function() {
+    row.addEventListener("dblclick", function () {
       const fullPath = this.getAttribute("data-path");
-      const parts = fullPath.split('/');
+      const parts = fullPath.split("/");
       const partition = parts[0][0];
-      const path = parts.slice(1).join('/');
+      const path = parts.slice(1).join("/");
       const tableId = otherTableId === "panel1" ? "panel2" : "panel1";
       fetchFolderContents(tableId, partition, path);
     });
@@ -123,10 +160,10 @@ function fetchFolderContents(panelId, partition, path) {
         tableBody.innerHTML = content;
         const otherTableId = panelId === "panel1" ? "panel2" : "panel1";
         addRowSelectionListeners(tableBody, otherTableId);
-        if(panelId === "panel1"){
-            pathPanel1 = `${partition}?path=${path}`;
-        }else{
-            pathPanel2 = `${partition}?path=${path}`;
+        if (panelId === "panel1") {
+          pathPanel1 = `${partition}?path=${path}`;
+        } else {
+          pathPanel2 = `${partition}?path=${path}`;
         }
       }
     })
@@ -136,76 +173,105 @@ function fetchFolderContents(panelId, partition, path) {
     });
 }
 function deleteSelectedFiles() {
-  const panels = ['panel1', 'panel2'];
+  const panels = ["panel1", "panel2"];
+  let currentPanelId = "";
   let pathsToDelete = [];
   let selectedRowsElements = [];
 
-  panels.forEach(panelId => {
-    const tableBody = document.getElementById(panelId).getElementsByTagName("tbody")[0];
+  panels.forEach((panelId) => {
+    const tableBody = document
+      .getElementById(panelId)
+      .getElementsByTagName("tbody")[0];
     const selectedRows = tableBody.querySelectorAll("tr.selected");
 
-    const paths = Array.from(selectedRows).map(row => {
+    const paths = Array.from(selectedRows).map((row) => {
       selectedRowsElements.push(row);
-      return row.getAttribute('data-path');
+      return row.getAttribute("data-path");
     });
-
+    if (paths.length) {
+      currentPanelId = panelId === "panel1" ? "panel2" : "panel1";
+    }
     pathsToDelete = pathsToDelete.concat(paths);
   });
-   pathsToDelete.forEach(row => {
-   console.log(row)
-   })
+  [path, partition, fetchPath, isPartition] = getPathInfo(currentPanelId);
 
   if (pathsToDelete.length > 0) {
-    fetch('/', {
-      method: 'DELETE',
+    fetch("/", {
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ paths: pathsToDelete }),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      selectedRowsElements.forEach(row => row.remove());
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        selectedRowsElements.forEach((row) => row.remove());
+        if (isPartition) {
+          loadPartitionData(currentPanelId, partition);
+        } else {
+          fetchFolderContents(currentPanelId, partition, fetchPath);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   } else {
     console.log("No files selected for deletion.");
   }
 }
 
-function createFolder(panelId){
-    let path = panelId === 'panel1' ? pathPanel1 : pathPanel2
-    const fetchPath = path.split('=')[1]
-    console.log(fetchPath)
-    const partition = path[0]
-    let isPartition = false
-    if(path.length === 1){
-        path = `folder/${path}?path=/newFolder`
-        isPartition = true;
-    }else{
-        path = `/folder/${path}/newFolder`
-    }
-    fetch(path,{
-    method: 'POST',
+function createFolder(panelId,folderName) {
+  [path, partition, fetchPath, isPartition] = getPathInfo(panelId,folderName);
+  const oppositePanelId = panelId === "panel1" ? "panel2" : "panel1";
+  [oppositePath, oppositePartition, oppositeFetchPath, oppositeIsPartition] =
+    getPathInfo(oppositePanelId);
+
+  fetch(path, {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      if(isPartition){
-        loadPartitionData(panelId,partition)
-        }
-      else{
-        fetchFolderContents(panelId,partition,fetchPath)
-      }
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      fetchDataByCurrentPath(panelId, partition, fetchPath, isPartition);
+      fetchDataByCurrentPath(
+        oppositePanelId,
+        oppositePartition,
+        oppositeFetchPath,
+        oppositeIsPartition
+      );
     })
     .catch((error) => {
-      console.error('Error:', error);
+      console.error("Error:", error);
     });
-
 }
 
+function getPathInfo(panelId,folderName='') {
+  let path = panelId === "panel1" ? pathPanel1 : pathPanel2;
+  const fetchPath = path.split("=")[1];
+  const partition = path[0];
+  let isPartition = false;
+  if (path.length === 1) {
+    path = `folder/${path}?path=/${folderName}`;
+    isPartition = true;
+  } else {
+    path = `/folder/${path}/${folderName}`;
+  }
+  return [path, partition, fetchPath, isPartition];
+}
+
+function fetchDataByCurrentPath(
+  panelId,
+  partition,
+  fetchPath,
+  isPartition = false
+) {
+  if (isPartition) {
+    loadPartitionData(panelId, partition);
+  } else {
+    fetchFolderContents(panelId, partition, fetchPath);
+  }
+}
